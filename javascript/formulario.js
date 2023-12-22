0b4bc100339b342e947d1f184864425de9b95281
Ivan <103539074+IvanYedro01@users.noreply.github.com>
2023-12-21 22:46:37 -0300

    Todas las medias queries

--- javascript/formulario.js
+++ javascript/formulario.js
@@ -6,6 +6,6 @@ $form.addEventListener('submit', handleSubmit)
 function handleSubmit(event) {
   event.preventDefault()
   const form = new FormData(this)
-  $buttonMailto.setAttribute('href', `mailto:me@leonidasesteban.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('message')}`)
+  $buttonMailto.setAttribute('href', `mailto:exiplast.pvc@gmail.com?subject=Nombre ${form.get('name')} &body=${form.get('message')}`)
   $buttonMailto.click()
 }
